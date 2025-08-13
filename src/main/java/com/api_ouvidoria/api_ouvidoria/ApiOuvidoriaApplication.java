8ef6974b4d7b8ed942136b209b48cfae26c8e7e0
Gabrielli Danker <107625531+gabsdnker@users.noreply.github.com>
2025-08-13 11:12:43 -0300

    Adicionando Spring Boot

---
+++ src/main/java/com/api_ouvidoria/api_ouvidoria/ApiOuvidoriaApplication.java
@@ -0,0 +1,13 @@
+package com.api_ouvidoria.api_ouvidoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOuvidoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOuvidoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api_ouvidoria/api_ouvidoria/ApiOuvidoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_ouvidoria.api_ouvidoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOuvidoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
